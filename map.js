4f16fbb1d497bfe7d5425ddb0f5cb6fa8cd6076b
Siwei Wang <siwei.wang@capco.com>
2019-03-25 22:12:04 -0400

    fix color schema not found

--- map.js
+++ map.js
@@ -5,10 +5,10 @@ height = +svg.attr("height");
 
 var color = d3.scaleThreshold()
     .domain(d3.range(100000, 1000000, 100000))
-    .range(d3.schemeGreens[9]);
+    .range(colorbrewer.Reds[9]);
 // http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson
 d3.json("nyc.json", function(error, nyc) {
-	
+
 	d3.csv("NY_neighborhoods_zillow_index.csv",function(error, pricedata){
 if (error) throw error;
 
@@ -48,7 +48,7 @@ svg.selectAll("path")
   .text(d.properties.neighborhood + "; Zindex: "+ priceByName[d.properties.neighborhood])
 
 })
-.on("mouseleave", function(d) { 
+.on("mouseleave", function(d) {
   d3.select(this)
   .style("stroke-width", .25)
   .style("stroke-dasharray", 1)
